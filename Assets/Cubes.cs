6374af41499c2adddb89c75e26021d11eb25ac94
Riona <riona.mcgillicuddy@gmail.com>
2015-10-25 16:16:24 -0400

    Transport Project Part 1

---
+++ Assets/Cubes.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cubes : MonoBehaviour {
+
+	public GameObject cubes;
+	int totalNumberCubes = 16;
+
+	// Use this for initialization
+	void Start () {
+		for(int cubesSpawned = 0; cubesSpawned < totalNumberCubes; cubesSpawned++){
+			Instantiate (cubes, new Vector2(1, 0), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Transport 3D/Assets/CubeBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeBehavior : MonoBehaviour {
+
+	public CubeSpawnArray aGameController;
+	
+
+
+	void OnMouseDown (){
+		aGameController.ProcessClickedCube (this.gameObject);
+		print ("Do the thing!");
+	
+	}
+
+	// Use this for initialization
+	void Start () {
+		aGameController = GameObject.Find ("GameControllerObject").GetComponent<CubeSpawnArray> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	foreach (GameObject oneCube in aGameController.allCubes) {
+			oneCube.GetComponent<Renderer>().material.color = Color.white;
+		}
+
+	}
+}
