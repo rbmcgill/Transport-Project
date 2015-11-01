c1ad83751066a0af5ff7a6b9e2eeae377e0cc67f
Riona <riona.mcgillicuddy@gmail.com>
2015-11-01 15:02:33 -0500

    More of Part 2
    Making the plane traceable

---
+++ Transport 3D/Assets/Aiplane.cs
@@ -0,0 +1,14 @@
+﻿
+
+public class Aiplane  {
+	public int x, y;
+	//set starting position in x,y coordinates
+	//public thing startingPlace = (0,8);
+
+	public bool activeAirplane = true;
+	//track if an airplane is active or not
+	//if airplane is active, make it red
+	//put the airplane in the top left corner
+
+	public int cargo, cargoCapacity;
+}
--- Transport 3D/Assets/CubeSpawnArray.cs
+++ Transport 3D/Assets/CubeSpawnArray.cs
@@ -3,32 +3,58 @@ using System.Collections;
 
 public class CubeSpawnArray : MonoBehaviour {
 
-	public GameObject[] allCubes;
+	public GameObject[,] allCubes;
 	public GameObject Cube;
+	public Aiplane airplane;
 
-	int totalNumberCubes = 16;
+	int gridWidth = 16;
+	int gridHeight = 9;
 
-	public void ProcessClickedCube (GameObject clickedCube) {
-	//this whole script can be called by other scripts but won't return anything when completed
-		foreach (GameObject oneCube in allCubes) {
-			oneCube.GetComponent<Renderer>().material.color = Color.black;
+	public void ProcessClickedCube (GameObject clickedCube, int x, int y) {
+		if (x == airplane.x && y == airplane.y && airplane.activeAirplane == false) {
+			airplane.activeAirplane = true;
+			clickedCube.GetComponent<Renderer> ().material.color = Color.yellow;
+		} 
+		else if (x == airplane.x && y == airplane.y && airplane.activeAirplane) {
+			airplane.activeAirplane = false;
+			clickedCube.GetComponent<Renderer> ().material.color = Color.red;
 		}
-		clickedCube.GetComponent<Renderer>().material.color = Color.red;
-		print (clickedCube.GetComponent<Renderer>().material.color);
+		else if (airplane.activeAirplane && (x != airplane.x || y != airplane.y)) {
+		}
+
 	}
+	//when player clicks on plane, make the color change/have airplane glow to show it's activated
+	//if player clicks active plane it deactivates (changes back to red/stop glowing)
+
+
+	//if there is no active airplane, don't do anything, no teleportation, nothing
+
+
 
 
 	// Use this for initialization
 	void Start () {
-		allCubes = new GameObject[totalNumberCubes];
-		for(int cubesSpawned = 0; cubesSpawned < totalNumberCubes; cubesSpawned++) {
-			allCubes[cubesSpawned] = (GameObject) Instantiate (Cube, new Vector3 (cubesSpawned*2, 0, 0), Quaternion.identity);
-		
-		}
+		allCubes = new GameObject[gridWidth,gridHeight];
+		for(int x = 0; x < gridWidth; x++) {
+			for (int y = 0; y < gridHeight; y++){
+			allCubes[x,y] = (GameObject) Instantiate (Cube, new Vector3 (x*2, y*2, 0), Quaternion.identity);
+			
+			allCubes[x,y].GetComponent<CubeBehavior>().x = x;
+			allCubes[x,y].GetComponent<CubeBehavior>().y = y;
+			}
+			//reaching into CubeBehavior script, which is tracking the plane position
+			//uses those coordinates in cubes spawned here (in allCubes)
 
+		}
+		airplane = new Aiplane ();
+		//does this take or make a new object from the type Airplane? What does that do/mean?
+		airplane.x = 0;
+		airplane.y = 8; 
+
+		allCubes [airplane.x, airplane.y].GetComponents<Renderer>().material.color = Color.red;
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
